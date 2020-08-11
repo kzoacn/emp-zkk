64e0f00d092edafd31e2016e05e0d3856c7e17fd
kzoacn <kzoacn@outlook.com>
2020-08-11 15:02:22 +0800

    nb

--- test/test_mpc.cpp
+++ test/test_mpc.cpp
@@ -9,46 +9,6 @@ const string circuit_file_location = macro_xstr(EMP_CIRCUIT_PATH);
 static char out3[] = "92b404e556588ced6c1acd4ebf053f6809f73a93";//bafbc2c87c33322603f38e06c3e0f79c1f1b1475";
 
 
- 
-
-	/*
-	NetIOMP<RepIO,nP> rio(party, port);
-	NetIOMP<RepIO,nP> rio2(party, port+2*(nP+1)*(nP+1)+1);
-	NetIOMP<RepIO,nP> *rios[2] = {&rio, &rio2};
-
-	for(int i=1;i<=nP;i++){
-		if(i==party)continue;
-		if(io.ios[i])
-			rio.ios[i]->recv_rec=io.ios[i]->recv_rec;
-		if(io.ios2[i])
-			rio.ios2[i]->recv_rec=io.ios2[i]->recv_rec;
-
-		if(io2.ios[i])
-			rio2.ios[i]->recv_rec=io2.ios[i]->recv_rec;
-		if(io2.ios2[i])
-			rio2.ios2[i]->recv_rec=io2.ios2[i]->recv_rec;
-	}
-	
-
-
-	prng.reseed(&seed);
-	CMPC<RepIO,nP>* mpc2 = new CMPC<RepIO,nP>(rios, &pool, party, &cf,prng);
-
-	mpc2->function_independent();
-	mpc2->function_dependent();
-	memset(in, false, 512);	
-	mpc2->online(in, out);
-	if(party == 1) { 
-		string res = "";
-		for(int i = 0; i < cf.n3; ++i)
-			res += (out[i]?"1":"0");
-		cout << hex_to_binary(string(out3))<<endl;
-		cout << res<<endl;
-		cout << (res == hex_to_binary(string(out3))? "GOOD!":"BAD!")<<endl<<flush;
-	}
-
-*/
-
 const static int nP = 6;
 const int open_num=2;
  
@@ -92,6 +52,7 @@ void prove_verify(bool *in,int in_len,block seed,NetIOMP<RecIO,nP> *ios[2],int p
 		// party send to j    ios->get(j,party<j)
 		// party recv from j  ios->get(j,j<party) 
 
+		unsigned long long count=0;
 		for(int r=0;r<2;r++)
 		for(int i=1;i<=nP;i++){
 			if(i==party)continue;
@@ -100,14 +61,17 @@ void prove_verify(bool *in,int in_len,block seed,NetIOMP<RecIO,nP> *ios[2],int p
 				int len=vec.size(); 
 				io->send_data(verifier,&len,sizeof(len));
 				io->send_data(verifier,vec.data(),len);
+				count+=len;
 			}
 			if(ios[r]->ios2[i]){
 				auto vec=ios[r]->ios2[i]->recv_rec;
 				int len=vec.size();  
 				io->send_data(verifier,&len,sizeof(len));
 				io->send_data(verifier,vec.data(),len);
+				count+=len;
 			}
 		}
+		cout<<"party "<<party<<" "<<count/1024/1024<<"MB"<<endl;
 
 	}else{
 		bool input[512];
@@ -252,10 +216,12 @@ void prove_verify(bool *in,int in_len,block seed,NetIOMP<RecIO,nP> *ios[2],int p
 int main(int argc, char** argv) {
 	int port, party;
 	parse_party_and_port(argv, &party, &port);
-
+	int in_len=512;
 
 	if(party==nP+1){
-		prove_verify(0,512,zero_block(),0,party,port);
+		double st=clock();
+		prove_verify(0,in_len,zero_block(),0,party,port);
+		cout<<"time : " <<(clock()-st)/CLOCKS_PER_SEC<<endl;
 		return 0;
 	}
 
@@ -267,13 +233,13 @@ int main(int argc, char** argv) {
 	file = circuit_file_location+"/sha-1.txt";
 	CircuitFile cf(file.c_str());
 
-	PRG prng; block seed=makeBlock(0,0); prng.reseed(&seed);
+	PRG prng; block seed; prng.random_block(&seed,1); prng.reseed(&seed);
 	CMPC<RecIO,nP>* mpc = new CMPC<RecIO,nP>(ios, &pool, party, &cf,prng);
 
 	mpc->function_independent();
 	mpc->function_dependent();
 	bool in[512]; bool out[160];
-	memset(in, false, 512);	
+	memset(in, false, in_len);	
 	mpc->online(in, out);
 	if(1) {
 		string res = "";
@@ -287,36 +253,9 @@ int main(int argc, char** argv) {
 
 
 
-	prove_verify(in,512,seed,ios,party,port);
+	prove_verify(in,in_len,seed,ios,party,port);
 	
-
-/*	int s=0;
-	// party send to j    ios->get(j,party<j)
-	// party recv from j  ios->get(j,j<party)
-	for(int r=0;r<1;r++)
-	for(int j=1;j<=nP;j++){
-			if(j==party)continue;
-			char dig[128];
-			memset(dig,0,sizeof dig);
-			
-			RecIO *in=ios[r]->get(j,party<j);	
-			in->send_hash.digest(dig);
-			s=0;
-			for(int k=0;k<10;k++)
-				s+=dig[k];
-			cout<<party<<"->"<<j<<" "<<s<<endl;
-			
-
-
-			in=ios[r]->get(j,j<party);				
-			in->recv_hash.digest(dig);
-			s=0;
-			for(int k=0;k<10;k++)
-				s+=dig[k];
-			cout<<party<<"<-"<<j<<" "<<s<<endl;
-
-	}
-*/
+ 
 
 	delete mpc;
 	return 0;
